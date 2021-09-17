870ef615819f20d7adb562d480d16ede69822a22
T.J. Telan <t.telan@gmail.com>
2021-09-17 16:32:24 -0700

    Trying cross-plat ci build from fluvio

---
+++ build-scripts/install_target.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$TARGET" = "armv7-unknown-linux-gnueabihf" ] || [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]; then
+    # Install cross if not installed
+    [[ -x "$(command -v cross)" ]] || cargo install cross
+    # must perform helm pkg since we dont have access to pkg
+    make helm_pkg
+fi
+
+if [ -n "$TARGET" ]; then
+    echo "loading target $TARGET"
+    rustup target add $TARGET
+fi
