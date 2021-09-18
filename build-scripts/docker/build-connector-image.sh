4651a6d70a2a2b712545f7672de65bd60cec98e0
T.J. Telan <t.telan@gmail.com>
2021-09-17 18:47:09 -0700

    fix ci, shellcheck errors

--- build-scripts/docker/build-connector-image.sh
+++ build-scripts/docker/build-connector-image.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
-
 # The target platform - Ex: x86_64-unknown-linux-musl
-readonly TARGET=${TARGET:?Set TARGET platform to build}
+readonly TARGET="${TARGET:?Set TARGET platform to build}"
 # The tag to build this Docker image with - Ex: 0.7.4-abcdef (where abcdef is a git commit)
-readonly COMMIT_HASH${COMMIT_HASH:-}
+readonly COMMIT_HASH="${COMMIT_HASH:-}"
 # The name of the connector - Ex: test-connector
-readonly CONNECTOR_NAME="${CONNECTOR_NAME:?Set CONNECTOR_NAME to build}"
+readonly CONNECTOR_NAME="${CONNECTOR_NAME:-test-connector}"
 # Default image name structure
-readonly IMAGE_NAME="${IMAGE_NAME:infinyon/fluvio-connect-$CONNECTOR_NAME}"
+readonly IMAGE_NAME="${IMAGE_NAME:-infinyon/fluvio-connect-$CONNECTOR_NAME}"
 
 function main() {
+
 
   if [ "$TARGET" = "aarch64-unknown-linux-musl" ]; then
     BUILD_ARGS="--build-arg CONNECTOR_NAME=${CONNECTOR_NAME} --build-arg ARCH=arm64v8/"
@@ -22,8 +22,10 @@ function main() {
   # Tag the image with a commit hash if we provide it
   if [[ -z "$COMMIT_HASH" ]];
   then
+    # shellcheck disable=SC2086
     docker build -t "$IMAGE_NAME" $BUILD_ARGS .
   else
+    # shellcheck disable=SC2086
     docker build -t "$IMAGE_NAME" -t "$IMAGE_NAME:$COMMIT_HASH-$TARGET" $BUILD_ARGS .
   fi
 
