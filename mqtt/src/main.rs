9fbd745f7c9492ca9161e54f5d53bf188058defc
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-09-28 14:06:02 -0400

    Updates to docker and makefile

--- mqtt/src/main.rs
+++ mqtt/src/main.rs
@@ -18,7 +18,8 @@ fn try_reconnect(cli: &MqttClient) -> bool {
 	false
 }
 
-fn main() -> Result<(), MqttConnectorError> {
+#[async_std::main]
+async fn main() -> Result<(), MqttConnectorError> {
     fluvio_future::subscriber::init_tracer(None);
     let mqtt_url = "mqtt.hsl.fi";
     let mqtt_topic = "/hfp/v2/journey/#";
@@ -32,14 +33,15 @@ fn main() -> Result<(), MqttConnectorError> {
     let rx = mqtt_client.start_consuming();
     mqtt_client.connect(None)?;
     mqtt_client.subscribe(mqtt_topic, mqtt_qos)?;
-    let fluvio_client = fluvio_future::task::run_block_on(fluvio::Fluvio::connect())?;
-    let producer = fluvio_future::task::run_block_on(fluvio_client.topic_producer(fluvio_topic))?;
+
+    let fluvio_client = fluvio::Fluvio::connect().await?;
+    let producer = fluvio_client.topic_producer(fluvio_topic).await?;
 
     for msg in rx.iter() {
         if let Some(msg) = msg {
             let mqtt_topic = msg.topic();
             let mqtt_payload = msg.payload();
-            fluvio_future::task::run_block_on(producer.send(mqtt_topic, mqtt_payload))?;
+            let _ = producer.send(mqtt_topic, mqtt_payload).await?;
 
         } else if mqtt_client.is_connected() || !try_reconnect(&mqtt_client) {
             break;
