d2b5a562b54d88b817ba1c26c368bd0430469304
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-10-01 13:07:42 -0400

    cargo fmt

--- mqtt/src/error.rs
+++ mqtt/src/error.rs
@@ -1,5 +1,6 @@
 use fluvio::FluvioError;
 use paho_mqtt::Error as MqttError;
+use structopt::clap::Error as ClapError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -8,4 +9,7 @@ pub enum MqttConnectorError {
     Mqtt(#[from] MqttError),
     #[error("Fluvio error: `{0:?}`.")]
     Fluvio(#[from] FluvioError),
+
+    #[error("Clap Error: `{0:#?}`.")]
+    Clap(#[from] ClapError),
 }
--- mqtt/src/main.rs
+++ mqtt/src/main.rs
@@ -3,6 +3,79 @@ use paho_mqtt::client::Client as MqttClient;
 use paho_mqtt::CreateOptions;
 mod error;
 use error::MqttConnectorError;
+use structopt::StructOpt;
+
+use schemars::{schema_for, JsonSchema};
+#[derive(StructOpt, Debug, JsonSchema)]
+struct ConnectorOpts {
+    #[structopt(short, long)]
+    qos: Option<i32>,
+
+    #[structopt(long)]
+    timeout: Option<u64>,
+
+    #[structopt(short, long)]
+    mqtt_url: String,
+
+    #[structopt(long)]
+    mqtt_topic: String,
+
+    #[structopt(long)]
+    fluvio_topic: String,
+}
+
+#[async_std::main]
+async fn main() -> Result<(), MqttConnectorError> {
+    fluvio_future::subscriber::init_tracer(None);
+
+    let opts = match ConnectorOpts::from_args_safe() {
+        Ok(opts) => opts,
+        Err(_e) => {
+            let arguments: Vec<String> = std::env::args().collect();
+            match arguments.get(1) {
+                Some(schema) if schema == "schema" => {
+                    let schema = schema_for!(ConnectorOpts);
+                    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+                    return Ok(());
+                }
+                _ => {
+                    ConnectorOpts::from_args();
+                    return Ok(());
+                }
+            }
+        }
+    };
+
+    let mqtt_qos = opts.qos.unwrap_or(0);
+    let mqtt_timeout_seconds = opts.timeout.unwrap_or(60);
+    let mqtt_url = opts.url; //"mqtt.hsl.fi";
+    let mqtt_topic = opts.mqtt_topic; //"/hfp/v2/journey/#";
+    let fluvio_topic = opts.fluvio_topic; //"mqtt";
+
+    let timeout = std::time::Duration::from_secs(mqtt_timeout_seconds);
+    let mut mqtt_client = MqttClient::new(CreateOptions::from(mqtt_url))?;
+    mqtt_client.set_timeout(timeout);
+    let rx = mqtt_client.start_consuming();
+    mqtt_client.connect(None)?;
+    mqtt_client.subscribe(&mqtt_topic, mqtt_qos)?;
+
+    let fluvio_client = fluvio::Fluvio::connect().await?;
+    let producer = fluvio_client.topic_producer(fluvio_topic).await?;
+
+    for msg in rx.iter() {
+        if let Some(msg) = msg {
+            let mqtt_topic = msg.topic();
+            let mqtt_payload = msg.payload();
+
+            let _ = producer.send(mqtt_topic, mqtt_payload).await?;
+        } else if mqtt_client.is_connected() || !try_reconnect(&mqtt_client) {
+            break;
+        }
+    }
+
+    println!("Hello, world!");
+    Ok(())
+}
 
 fn try_reconnect(cli: &MqttClient) -> bool {
     println!("Connection lost. Waiting to retry connection");
@@ -16,37 +89,4 @@ fn try_reconnect(cli: &MqttClient) -> bool {
     }
     println!("Unable to reconnect after several attempts.");
     false
-}
-
-#[async_std::main]
-async fn main() -> Result<(), MqttConnectorError> {
-    fluvio_future::subscriber::init_tracer(None);
-    let mqtt_url = "mqtt.hsl.fi";
-    let mqtt_topic = "/hfp/v2/journey/#";
-    let mqtt_qos = 0;
-    let mqtt_timeout_seconds = 60;
-    let fluvio_topic = "mqtt";
-
-    let timeout = std::time::Duration::from_secs(mqtt_timeout_seconds);
-    let mut mqtt_client = MqttClient::new(CreateOptions::from(mqtt_url))?;
-    mqtt_client.set_timeout(timeout);
-    let rx = mqtt_client.start_consuming();
-    mqtt_client.connect(None)?;
-    mqtt_client.subscribe(mqtt_topic, mqtt_qos)?;
-
-    let fluvio_client = fluvio::Fluvio::connect().await?;
-    let producer = fluvio_client.topic_producer(fluvio_topic).await?;
-
-    for msg in rx.iter() {
-        if let Some(msg) = msg {
-            let mqtt_topic = msg.topic();
-            let mqtt_payload = msg.payload();
-            let _ = producer.send(mqtt_topic, mqtt_payload).await?;
-        } else if mqtt_client.is_connected() || !try_reconnect(&mqtt_client) {
-            break;
-        }
-    }
-
-    println!("Hello, world!");
-    Ok(())
 }
