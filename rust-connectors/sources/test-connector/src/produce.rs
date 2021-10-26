215d313e7e8fc0af2bf29cb4cedf8cdfc7563f05
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-10-26 13:17:51 -0400

    Added some simple integration testing patterns

--- rust-connectors/sources/test-connector/src/produce.rs
+++ rust-connectors/sources/test-connector/src/produce.rs
@@ -1,53 +1,34 @@
 use crate::opts::TestConnectorOpts;
 use fluvio::RecordKey;
-use fluvio_dataplane_protocol::smartstream::{SmartStreamExtraParams, SmartStreamInput};
-use fluvio_smartengine::{SmartEngine, SmartStream};
+use fluvio::metadata::smartmodule::SmartModuleSpec;
 
 pub async fn produce(opts: TestConnectorOpts) -> Result<(), fluvio::FluvioError> {
-    let producer = fluvio::producer(opts.topic).await?;
+
+    let mut producer = fluvio::producer(opts.topic).await?;
     let num_records = opts.count.unwrap_or(i64::MAX);
     let timeout = opts.timeout.unwrap_or(1000);
 
-    let engine = SmartEngine::default();
-    let mut smart_stream: Option<Box<dyn SmartStream>> = None;
-    if let Some(wasm_path) = opts.smartstream_filter {
-        let smart_stream_module = engine
-            .create_module_from_path(wasm_path)
-            .expect("Failed to read wasm path");
-        smart_stream = Some(Box::new(
-            smart_stream_module
-                .create_filter(&engine, SmartStreamExtraParams::default())
-                .expect("Failed to create smart stream filter"),
-        ));
+    if let Some(smart_module) = opts.smartstream_filter {
+        let admin = fluvio::FluvioAdmin::connect().await?;
+        let modules = admin.list::<SmartModuleSpec, _>(vec![smart_module.clone()]).await?;
+        let module = modules.first().expect(&format!("Failed to find smartmodule for {}", smart_module));
+        let wasm = &module.spec.wasm;
+        producer = producer.wasm_filter(wasm.payload.clone(), Default::default())
     }
 
-    if let Some(wasm_path) = opts.smartstream_map {
-        let smart_stream_module = engine
-            .create_module_from_path(wasm_path)
-            .expect("Failed to read wasm path");
-        smart_stream = Some(Box::new(
-            smart_stream_module
-                .create_map(&engine, SmartStreamExtraParams::default())
-                .expect("Failed to create smart stream filter"),
-        ));
+    if let Some(smart_module) = opts.smartstream_map {
+        let admin = fluvio::FluvioAdmin::connect().await?;
+        let modules = admin.list::<SmartModuleSpec, _>(vec![smart_module.clone()]).await?;
+        let module = modules.first().expect(&format!("Failed to find smartmodule for {}", smart_module));
+        let wasm = &module.spec.wasm;
+
+        producer = producer.wasm_map(wasm.payload.clone(), Default::default())
     }
 
     for i in 1..num_records {
         let value = format!("Hello, Fluvio! - {}", i);
-
         println!("Sending {}", value);
-        if let Some(ref mut smart_stream) = smart_stream {
-            let input = SmartStreamInput::from_single_record(value.as_bytes())
-                .expect("Failed to get smartstream input from value");
-            let output = smart_stream
-                .process(input)
-                .expect("Failed to process record");
-            for i in &output.successes {
-                producer.send(RecordKey::NULL, i.value().clone()).await?;
-            }
-        } else {
-            producer.send(RecordKey::NULL, value).await?;
-        }
+        producer.send(RecordKey::NULL, value).await?;
         async_std::task::sleep(std::time::Duration::from_millis(timeout)).await;
     }
     Ok(())
