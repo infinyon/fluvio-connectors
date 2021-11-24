8a8987768378fae701e4fd40ac282b40867fc087
Sebastian Imlay <sebastian.imlay@gmail.com>
2021-11-24 14:55:14 -0500

    Moved to use different async runtime

--- rust-connectors/sources/mqtt/src/main.rs
+++ rust-connectors/sources/mqtt/src/main.rs
@@ -45,8 +45,8 @@ enum ConnectorDirection {
 }
 
 //#[async_std::main]
-#[tokio::main]
-async fn main() -> Result<(), MqttConnectorError> {
+//#[tokio::main]
+fn main() -> Result<(), MqttConnectorError> {
     let arguments: Vec<String> = std::env::args().collect();
     let opts: MqttOpts = match arguments.get(1) {
         Some(schema) if schema == "metadata" => {
@@ -70,42 +70,43 @@ async fn main() -> Result<(), MqttConnectorError> {
     }
     tracing::info!("Initializing MQTT connector");
 
-    let mqtt_qos = opts.qos.unwrap_or(0);
-    let mqtt_timeout_seconds = opts.timeout.unwrap_or(60);
-    let mqtt_url = opts.mqtt_url; //"mqtt.hsl.fi";
-    let mqtt_topic = opts.mqtt_topic; //"/hfp/v2/journey/#";
 
-    tracing::info!(
-        "Using timeout={}s, url={}, fluvio-topic={}, mqtt-topic={}",
-        mqtt_timeout_seconds,
-        mqtt_url,
-        opts.common.fluvio_topic,
-        mqtt_topic,
-    );
+    async_global_executor::block_on(async move {
+        let mqtt_qos = opts.qos.unwrap_or(0);
+        let mqtt_timeout_seconds = opts.timeout.unwrap_or(60);
+        let mqtt_url = opts.mqtt_url; //"mqtt.hsl.fi";
+        let mqtt_topic = opts.mqtt_topic; //"/hfp/v2/journey/#";
 
-    let timeout = std::time::Duration::from_secs(mqtt_timeout_seconds);
+        tracing::info!(
+            "Using timeout={}s, url={}, fluvio-topic={}, mqtt-topic={}",
+            mqtt_timeout_seconds,
+            mqtt_url,
+            opts.common.fluvio_topic,
+            mqtt_topic,
+        );
 
-    use rumqttc::{MqttOptions, AsyncClient, QoS};
-    let mut mqttoptions = MqttOptions::new("rumqtt-async", mqtt_url, 1883);
-    mqttoptions.set_keep_alive(timeout);
+        let timeout = std::time::Duration::from_secs(mqtt_timeout_seconds);
 
-    let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-    client.subscribe(mqtt_topic, QoS::AtMostOnce).await?;
+        use rumqttc::{MqttOptions, AsyncClient, QoS};
+        let mut mqttoptions = MqttOptions::new("rumqtt-async", mqtt_url, 1883);
+        mqttoptions.set_keep_alive(timeout);
 
-    loop {
-        let notification = eventloop.poll().await?;
-        println!("Received = {:?}", notification);
-        if let Ok(mqtt_event) = MqttEvent::try_from(notification) {
-            let fluvio_record = serde_json::to_string(&mqtt_event).unwrap();
+        let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+        client.subscribe(mqtt_topic, QoS::AtMostOnce).await?;
+        let producer = opts.common.create_producer().await?;
+        tracing::info!("Connected to Fluvio");
+
+        loop {
+            let notification = eventloop.poll().await?;
+            if let Ok(mqtt_event) = MqttEvent::try_from(notification) {
+                let fluvio_record = serde_json::to_string(&mqtt_event).unwrap();
+                debug!("Record before smartstream {}", fluvio_record);
+                producer.send(RecordKey::NULL, fluvio_record).await?;
+            }
         }
 
-    }
+    })
 
-    let producer = opts.common.create_producer().await?;
-    tracing::info!("Connected to Fluvio");
-
-
-    Ok(())
 }
 
 #[derive(Debug, Serialize)]
