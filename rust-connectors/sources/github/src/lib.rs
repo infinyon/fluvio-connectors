102b78403b57233c406e63e6ea5a421ed0c04176
Nick Mosher <nicholastmosher@gmail.com>
2021-12-08 09:15:13 -0500

    Begin pagination

--- rust-connectors/sources/github/src/lib.rs
+++ rust-connectors/sources/github/src/lib.rs
@@ -1,5 +1,7 @@
 use reqwest::{Method, Url};
+use std::time::Duration;
 use structopt::StructOpt;
+use tokio::time::sleep;
 
 pub type Result<T> = color_eyre::Result<T>;
 
@@ -9,6 +11,10 @@ pub struct GitHubOpt {
     repositories: Vec<String>,
     #[structopt(long, env, hide_env_values = true)]
     github_access_token: String,
+
+    /// Fetch elements that have been updated_at a more recent time than start_date
+    #[structopt(long)]
+    start_date: Option<String>,
 }
 
 pub struct GitHubConnector {
@@ -22,24 +28,41 @@ impl GitHubConnector {
         Ok(Self { client, config })
     }
 
-    pub async fn send_request(&self) -> Result<()> {
-        let res = self
-            .client
-            .request(
-                Method::GET,
-                Url::parse("https://api.github.com/repos/infinyon/infinyon-website").unwrap(),
-            )
-            .header("User-Agent", "fluvio-connect-github")
-            .header("Accept", "application/vnd.github.v3+json")
-            .header(
-                "Authorization",
-                format!("token {}", self.config.github_access_token),
-            )
-            .send()
-            .await?;
+    pub async fn run_stream(&self) -> Result<()> {
+        for i in 1.. {
+            let mut req = self
+                .client
+                .request(
+                    Method::GET,
+                    Url::parse("https://api.github.com/repos/infinyon/fluvio/issues").unwrap(),
+                )
+                .header("User-Agent", "fluvio-connect-github")
+                .header("Accept", "application/vnd.github.v3+json")
+                .header(
+                    "Authorization",
+                    format!("token {}", self.config.github_access_token),
+                )
+                .query(&[
+                    ("state", "all"),
+                    ("direction", "asc"),
+                    ("page", &i.to_string()),
+                ]);
 
-        let text = res.text().await?;
-        println!("Response: {}", text);
+            let res = req.send().await?;
+
+            eprintln!("Response Headers: {:#?}", res.headers());
+            let json: serde_json::Value = res.json().await?;
+            println!("{}", json);
+
+            let json_array = json.as_array().unwrap();
+            if json_array.is_empty() {
+                eprintln!("Hit empty page");
+                return Ok(());
+            }
+
+            sleep(Duration::from_millis(1_000)).await;
+        }
+
         Ok(())
     }
 }
