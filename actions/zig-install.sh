b38274b04b999fe7ce55f25b36c594d410c93432
T.J. Telan <t.telan@gmail.com>
2022-01-13 19:04:48 -0800

    Update zig install script
    Fix shellcheck errs on build script

--- actions/zig-install.sh
+++ actions/zig-install.sh
@@ -8,15 +8,15 @@ if [[ "$MATRIX_OS" == "ubuntu-latest" ]]; then
     echo "installing zig on ubuntu"
     sudo snap install --beta --classic zig && \
     sudo apt-get install lld-11 && \
-    echo "FLUVIO_BUILD_LLD=lld-11" | tee -a $GITHUB_ENV
+    echo "FLUVIO_BUILD_LLD=lld-13" | tee -a $GITHUB_ENV
 fi
 
 if [[ "$MATRIX_OS" == "macos-latest" ]]; then
     echo "installing zig on mac"
  #   brew update
     brew install zig && \
-    brew install llvm@11 && \
-    echo "FLUVIO_BUILD_LLD=/usr/local/opt/llvm@11/bin/lld" | tee -a $GITHUB_ENV
+    brew install llvm@13 && \
+    echo "FLUVIO_BUILD_LLD=/usr/local/opt/llvm@13/bin/lld" | tee -a $GITHUB_ENV
 fi
 
 
