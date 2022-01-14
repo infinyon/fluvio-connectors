13eefa3725cdebcfbd644c74317c6e6b0238179b
T.J. Telan <t.telan@gmail.com>
2022-01-13 18:50:59 -0800

    Move container build around for alternative Dockerfiles

--- build-scripts/docker/build-connector-image.sh
+++ build-scripts/docker/build-connector-image.sh
@@ -10,8 +10,24 @@ readonly CONNECTOR_NAME="${CONNECTOR_NAME:-test-connector}"
 # Default image name structure
 readonly IMAGE_NAME="infinyon/fluvio-connect-$CONNECTOR_NAME"
 
+# Creates a staging Docker build directory
+readonly BUILD_ROOT="$(realpath $( dirname ${BASH_SOURCE[0]})/../../container-build)"
+readonly WORK_DIR="$(mktemp -d -p $BUILD_ROOT)"
+
+# Default path to Dockerfile (for `docker build`) 
+readonly DOCKERFILE_PATH="$(realpath ${DOCKERFILE_PATH:-$BUILD_ROOT/dockerfiles/default/Dockerfile})"
+
+# check if tmp dir was created
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
 function main() {
 
+  # We need to make sure we always know what dir we are in
+  pushd "$BUILD_ROOT"
+  ls
 
   if [ "$TARGET" = "aarch64-unknown-linux-musl" ]; then
     BUILD_ARGS="--platform linux/arm64 --build-arg CONNECTOR_NAME=${CONNECTOR_NAME} --build-arg ARCH=arm64v8/"
@@ -28,19 +44,33 @@ function main() {
     IMAGE_TAGS="-t $IMAGE_NAME -t $IMAGE_NAME:$DOCKER_TAG-${TARGET}"
   fi
   DOCKER_IMAGE_TAR=/tmp/infinyon-fluvio-connector-${CONNECTOR_NAME}-${TARGET}.tar
+
+  # Copy Dockerfile and any other files copied to $BUILD_ROOT to $WORK_DIR
+  cp $DOCKERFILE_PATH $WORK_DIR 
+  find . -maxdepth 1 -type f -exec cp {} $WORK_DIR \;
+
+  # Changes directory then runs appropriate docker build command
+  pushd $WORK_DIR
+  ls
+
   # The CI build should producer a tarball
   if [[ -z "$CI" ]];
   then
     # shellcheck disable=SC2086
-    docker build $IMAGE_TAGS $BUILD_ARGS .
+    docker build $IMAGE_TAGS $BUILD_ARGS . 
     docker save ${IMAGE_NAME} > ${DOCKER_IMAGE_TAR}
     k3d image import -k -c fluvio ${DOCKER_IMAGE_TAR}
   else
     # shellcheck disable=SC2086
-    docker buildx build -o type=docker,dest=- $IMAGE_TAGS $BUILD_ARGS . > ${DOCKER_IMAGE_TAR}
+    docker buildx build -o type=docker,dest=- $IMAGE_TAGS $BUILD_ARGS $DOCKERFILE_PATH > ${DOCKER_IMAGE_TAR}
   fi
+  popd
 
 
+  ## Cleans up staging directory
+  popd
+  echo "Cleaning up work dir"
+  rm -rf $WORK_DIR
 
 }
 
