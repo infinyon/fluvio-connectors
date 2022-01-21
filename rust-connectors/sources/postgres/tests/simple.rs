aa1f1baa25d82e6259e15f24f2f27e0d098544d9
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-01-21 14:57:55 -0500

    cargo fmt

--- rust-connectors/sources/postgres/tests/simple.rs
+++ rust-connectors/sources/postgres/tests/simple.rs
@@ -18,7 +18,11 @@ async fn postgres_simple() -> eyre::Result<()> {
 
     let admin = fluvio::FluvioAdmin::connect().await?;
     let _ = admin
-        .create(fluvio_topic.clone(), false,  TopicSpec::new_computed(1, 1, Some(false)))
+        .create(
+            fluvio_topic.clone(),
+            false,
+            TopicSpec::new_computed(1, 1, Some(false)),
+        )
         .await;
     let config = PgConnectorOpt {
         url: Url::parse(&postgres_url).expect("Failed to parse connector url"),
