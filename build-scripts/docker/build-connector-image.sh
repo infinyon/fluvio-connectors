a6642b3acafd5c04de132ed6ede573470ed3163f
T.J. Telan <t.telan@gmail.com>
2022-01-20 22:01:21 -0800

    Test CI w/ new fluvio

--- build-scripts/docker/build-connector-image.sh
+++ build-scripts/docker/build-connector-image.sh
@@ -39,9 +39,9 @@ function main() {
   # Tag the image with a commit hash if we provide it
   if [[ -z "$DOCKER_TAG" ]];
   then
-    IMAGE_TAGS="-t $IMAGE_NAME"
+    IMAGE_TAGS="-t $IMAGE_NAME:latest"
   else
-    IMAGE_TAGS="-t $IMAGE_NAME -t $IMAGE_NAME:$DOCKER_TAG-${TARGET}"
+    IMAGE_TAGS="-t $IMAGE_NAME:latest -t $IMAGE_NAME:$DOCKER_TAG-${TARGET}"
   fi
   DOCKER_IMAGE_TAR=/tmp/infinyon-fluvio-connector-${CONNECTOR_NAME}-${TARGET}.tar
 
