56403ac254abd36469b18bc9e444e5436d224ab6
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-06-08 11:48:46 -0400

    see if CI passes

--- rust-connectors/sinks/kafka/src/main.rs
+++ rust-connectors/sinks/kafka/src/main.rs
@@ -1,5 +1,5 @@
 use fluvio_connectors_common::opt::{CommonSourceOpt, Record};
-use fluvio_future::tracing::info;
+use fluvio_future::tracing::{debug, info};
 use schemars::schema_for;
 use schemars::JsonSchema;
 use structopt::StructOpt;
@@ -37,58 +37,46 @@ pub struct KafkaOpt {
     pub common: CommonSourceOpt,
 }
 
+use rdkafka::config::ClientConfig;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use std::time::Duration;
+
 impl KafkaOpt {
     pub async fn execute(&self) -> anyhow::Result<()> {
         let mut stream = self.common.create_consumer_stream().await?;
 
-        info!("Starting stream");
-        while let Some(Ok(record)) = stream.next().await {
-            let _ = self.send_to_kafka(&record).await?;
-        }
-        Ok(())
-    }
-    pub async fn send_to_kafka(
-        &self,
-        _record: &Record,
-        //kafka_client: &mut KafkaClient,
-    ) -> anyhow::Result<()> {
-        todo!()
-    }
-    /*
-    pub async fn execute(&self) -> anyhow::Result<()> {
-        let mut stream = self.common.create_consumer_stream().await?;
-        let mut kafka_client = KafkaClient::new(vec![self.kafka_url.clone()]);
-        //let mut kafka_client = KafkaClient::new(vec![self.kafka_url.clone()]);
-        let _ = kafka_client.load_metadata_all()?;
+        let producer: &FutureProducer = &ClientConfig::new()
+            .set("bootstrap.servers", "")
+            .set("security.protocol", "SASL_SSL")
+            .set("sasl.mechanisms", "PLAIN")
+            .set("sasl.username", "")
+            .set("sasl.password", "")
+            .set("session.timeout.ms", "45000")
+            .create()
+            .expect("Producer creation error");
 
         info!("Starting stream");
         while let Some(Ok(record)) = stream.next().await {
-            let _ = self.send_to_kafka(&record, &mut kafka_client).await?;
+            let _ = self.send_to_kafka(&record, producer).await?;
         }
         Ok(())
     }
     pub async fn send_to_kafka(
         &self,
         record: &Record,
-        kafka_client: &mut KafkaClient,
+        kafka_producer: &FutureProducer,
     ) -> anyhow::Result<()> {
         let kafka_topic = self
             .kafka_topic
             .as_ref()
             .unwrap_or(&self.common.fluvio_topic);
-        let text = String::from_utf8_lossy(record.value());
-        debug!("Sending {:?}, to kafka", text);
-        let msg = ProduceMessage::new(
-            kafka_topic,
-            self.common.fluvio_partition,
-            record.key(),
-            Some(record.value()),
-        );
-        let req = vec![msg];
-        let resp =
-            kafka_client.produce_messages(RequiredAcks::All, Duration::from_millis(10000), req)?;
-        debug!("Sent {:?}", resp);
+        let kafka_record = FutureRecord::to(kafka_topic.as_str())
+            .payload(record.value())
+            .key(record.key().unwrap_or(&[]));
+        let res = kafka_producer
+            .send(kafka_record, Duration::from_secs(0))
+            .await;
+        debug!("Kafka produce {:?}", res);
         Ok(())
     }
-    */
 }
