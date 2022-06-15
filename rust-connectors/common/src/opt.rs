7dd79ce951811d6a99f7ec478a83a4eb902cc0e1
Sebastian Imlay <sebastian.imlay@gmail.com>
2022-06-15 12:47:30 -0400

    fix clippy

--- rust-connectors/common/src/opt.rs
+++ rust-connectors/common/src/opt.rs
@@ -159,7 +159,7 @@ impl CommonSourceOpt {
                 producer.with_filter(data, Default::default())?
             }
             (_, Some(filter_map_path), _, _, _) => {
-                let data = self.get_smartmodule(filter_map_path, &fluvio).await?;
+                let _data = self.get_smartmodule(filter_map_path, &fluvio).await?;
                 todo!("Filter map isn't in fluvio yet. https://github.com/infinyon/fluvio-connectors/issues/272");
                 //producer.with_filter_map(data, Default::default())?
             }
