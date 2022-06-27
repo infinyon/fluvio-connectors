514a8aa4908f43ea21ff3c0d6e691fea34382109
Luis Moreno <morenol@users.noreply.github.com>
2022-06-27 13:21:19 -0400

    Use extra parameters in connector smartmodule

--- rust-connectors/common/src/lib.rs
+++ rust-connectors/common/src/lib.rs
@@ -3,6 +3,7 @@ pub mod fluvio {
     pub use fluvio::{FluvioError, PartitionConsumer, RecordKey, TopicProducer};
 }
 
+pub(crate) mod error;
 pub mod opt;
 
 pub fn git_hash_version() -> &'static str {
