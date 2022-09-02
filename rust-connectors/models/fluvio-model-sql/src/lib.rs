938d027248516ba30fd8a09e39139ea046df299b
Alexander Galibey <48586936+galibey@users.noreply.github.com>
2022-09-02 22:08:37 +0300

    feat(smartmodules): added `json-sql` SmartModule (#329)
    * feat(smartmodules): added `json-sql` SmartModule

---
+++ rust-connectors/models/fluvio-model-sql/src/lib.rs
@@ -0,0 +1,44 @@
+use serde::Deserialize;
+use serde::Serialize;
+
+/// Top-level list of supported operations in the SQL model.
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub enum Operation {
+    Insert { table: String, values: Vec<Value> },
+}
+
+/// Value with SQL column name and supported SQL type.
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub struct Value {
+    pub column: String,
+    pub raw_value: String,
+    #[serde(rename = "type")]
+    pub type_: Type,
+}
+
+/// Supported SQL data types.
+#[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+pub enum Type {
+    Bool,
+    Char,
+
+    SmallInt,
+    Int,
+    BigInt,
+
+    Float,
+    DoublePrecision,
+
+    Text,
+    Bytes,
+
+    Numeric,
+
+    Timestamp,
+    Date,
+    Time,
+
+    Uuid,
+
+    Json,
+}
---
+++ smartmodules/json-sql/src/lib.rs
@@ -0,0 +1,47 @@
+mod mapping;
+mod pointer;
+mod transform;
+
+use once_cell::sync::OnceCell;
+
+use crate::mapping::Mapping;
+use eyre::ContextCompat;
+use fluvio_smartmodule::{
+    dataplane::smartmodule::{SmartModuleExtraParams, SmartModuleInternalError},
+    smartmodule, Record, RecordData, Result,
+};
+
+static MAPPING: OnceCell<Mapping> = OnceCell::new();
+
+#[smartmodule(init)]
+fn init(params: SmartModuleExtraParams) -> i32 {
+    if let Some(raw_mapping) = params.get("mapping") {
+        match serde_json::from_str(raw_mapping) {
+            Ok(mapping) => {
+                MAPPING
+                    .set(mapping)
+                    .expect("mapping is already uninitialized");
+                0
+            }
+            Err(err) => {
+                eprintln!("unable to parse init params: {:?}", err);
+                SmartModuleInternalError::InitParamsParse as i32
+            }
+        }
+    } else {
+        SmartModuleInternalError::InitParamsNotFound as i32
+    }
+}
+
+#[smartmodule(map)]
+pub fn map(record: &Record) -> Result<(Option<RecordData>, RecordData)> {
+    let mapping = MAPPING
+        .get()
+        .wrap_err("using of uninitialized mapping. Forgot call constructor?")?;
+
+    let key = record.key.clone();
+    let record = serde_json::from_slice(record.value.as_ref())?;
+    let transformed = transform::transform(record, mapping)?;
+
+    Ok((key, serde_json::to_vec(&transformed)?.into()))
+}
