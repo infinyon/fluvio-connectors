7d449dee06004b3ee8c4b037bfcb43a4d429755a
Alexander Galibey <alexander.galibey@infinyon.com>
2022-09-20 11:07:39 -0400

    feat(mqtt): added `json` payload output type

--- rust-connectors/sources/mqtt/src/main.rs
+++ rust-connectors/sources/mqtt/src/main.rs
@@ -1,53 +1,23 @@
 use fluvio_connectors_common::fluvio::RecordKey;
 use fluvio_connectors_common::git_hash_version;
-use fluvio_connectors_common::opt::CommonConnectorOpt;
 
 mod error;
+mod formatter;
+mod opt;
+
 use error::MqttConnectorError;
 
+use crate::opt::{ConnectorDirection, MqttOpts};
 use clap::Parser;
 use fluvio_future::tracing::{debug, error, info};
 use rumqttc::{v4::Packet, AsyncClient, ClientConfig, Event, MqttOptions, QoS, Transport};
-use schemars::{schema_for, JsonSchema};
+use schemars::schema_for;
+use serde::Deserialize;
 use serde::Serialize;
 use std::convert::TryFrom;
 use std::time::Duration;
 use url::Url;
 
-#[derive(Parser, Debug, JsonSchema)]
-struct MqttOpts {
-    #[clap(long)]
-    timeout: Option<u64>,
-
-    #[clap(short, long, env = "MQTT_URL", hide_env_values = true)]
-    mqtt_url: String,
-
-    #[clap(long)]
-    mqtt_topic: String,
-
-    #[clap(long)]
-    client_id: Option<String>,
-
-    #[clap(flatten)]
-    #[schemars(flatten)]
-    common: CommonConnectorOpt,
-}
-
-#[derive(Debug, Serialize)]
-struct MySchema {
-    name: &'static str,
-    direction: ConnectorDirection,
-    schema: schemars::schema::RootSchema,
-    version: &'static str,
-    description: &'static str,
-}
-
-#[derive(Debug, Serialize)]
-#[allow(dead_code)] // The other variants aren't used but are part of the spec.
-enum ConnectorDirection {
-    Source,
-    Sink,
-}
 fn main() -> Result<(), MqttConnectorError> {
     let arguments: Vec<String> = std::env::args().collect();
     let opts: MqttOpts = match arguments.get(1) {
@@ -73,6 +43,8 @@ fn main() -> Result<(), MqttConnectorError> {
         git_hash = git_hash_version(),
         "Starting MQTT source connector",
     );
+
+    debug!("opts={:?}", opts);
 
     async_global_executor::block_on(async move {
         let mqtt_timeout_seconds = Duration::from_secs(opts.timeout.unwrap_or(60));
@@ -117,13 +89,13 @@ fn main() -> Result<(), MqttConnectorError> {
             mqttoptions.set_transport(Transport::tls_with_config(client_config.into()));
         }
         let producer = opts.common.create_producer().await?;
-
+        let formatter = formatter::from_output_type(&opts.payload_output_type);
         loop {
             let (client, mut eventloop) = AsyncClient::new(mqttoptions.clone(), 10);
             client
                 .subscribe(mqtt_topic.clone(), QoS::AtMostOnce)
                 .await?;
-            tracing::info!("Connected to Fluvio");
+            info!("Connected to Fluvio");
             loop {
                 let notification = match eventloop.poll().await {
                     Ok(notification) => notification,
@@ -133,7 +105,7 @@ fn main() -> Result<(), MqttConnectorError> {
                     }
                 };
                 if let Ok(mqtt_event) = MqttEvent::try_from(notification) {
-                    let fluvio_record = serde_json::to_string(&mqtt_event)?;
+                    let fluvio_record = formatter.to_string(&mqtt_event)?;
                     debug!("Record before smartstream {}", fluvio_record);
                     if let Err(e) = producer.send(RecordKey::NULL, fluvio_record).await {
                         error!("Fluvio error! {}", e);
@@ -142,20 +114,29 @@ fn main() -> Result<(), MqttConnectorError> {
                     }
                 }
             }
-            fluvio_future::timer::sleep(std::time::Duration::from_secs(5)).await;
+            fluvio_future::timer::sleep(Duration::from_secs(5)).await;
             debug!("reconnecting to mqtt and fluvio");
         }
     })
 }
 
 #[derive(Debug, Serialize)]
+struct MySchema {
+    name: &'static str,
+    direction: ConnectorDirection,
+    schema: schemars::schema::RootSchema,
+    version: &'static str,
+    description: &'static str,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct MqttEvent {
     mqtt_topic: String,
     payload: Vec<u8>,
 }
-impl TryFrom<rumqttc::Event> for MqttEvent {
+impl TryFrom<Event> for MqttEvent {
     type Error = String;
-    fn try_from(event: rumqttc::Event) -> Result<Self, Self::Error> {
+    fn try_from(event: Event) -> Result<Self, Self::Error> {
         match event {
             Event::Incoming(Packet::Publish(p)) => Ok(Self {
                 mqtt_topic: p.topic,
