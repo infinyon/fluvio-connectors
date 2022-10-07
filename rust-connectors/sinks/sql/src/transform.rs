85d8958e557835261810dc954de6a811c08157ff
Adrian Black <adrian@infinyon.com>
2022-10-07 16:07:42 -0700

    load module from local cluster, remove hub download logic

--- rust-connectors/sinks/sql/src/transform.rs
+++ rust-connectors/sinks/sql/src/transform.rs
@@ -1,11 +1,11 @@
-use crate::download::Downloader;
 use crate::opt::TransformOpt;
 use fluvio::dataplane::record::Record;
 use fluvio_smartengine::{SmartEngine, SmartModuleChainInstance, SmartModuleConfig};
 
+use fluvio::Fluvio;
+use fluvio_connectors_common::opt::CommonConnectorOpt;
 use fluvio_smartmodule::dataplane::smartmodule::SmartModuleInput;
 use std::collections::BTreeMap;
-use url::Url;
 
 const PARAM_WITH: &str = "with";
 
@@ -15,23 +15,26 @@ pub struct Transformations {
 }
 
 impl Transformations {
-    pub async fn from_hub(
-        hub_url: Url,
-        value: Vec<TransformOpt>,
-    ) -> anyhow::Result<Transformations> {
+    // TODO: move up to common crate
+    pub async fn from_fluvio(value: Vec<TransformOpt>) -> anyhow::Result<Transformations> {
         let mut builder = SmartEngine::new().builder();
         if value.is_empty() {
             return Ok(Self {
                 smart_module_chain: builder.initialize()?,
             });
         }
-        let downloader = Downloader::from_url(hub_url)?;
+
+        let fluvio = Fluvio::connect().await?;
+
         for step in value {
             let mut param: BTreeMap<String, String> = BTreeMap::new();
             if let Some(with) = step.with {
                 param.insert(PARAM_WITH.to_string(), with);
             }
-            let raw = downloader.download_binary(step.uses.as_str()).await?;
+            let raw = CommonConnectorOpt::default()
+                .get_smartmodule(&step.uses, &fluvio)
+                .await?;
+
             let mut sm_builder = SmartModuleConfig::builder();
             let sm_builder = sm_builder.params(param.into());
 
