25036519aa1aa51475abcacaebc1c277cf5f87e1
Adrian Black <adrian@infinyon.com>
2022-10-07 16:07:42 -0700

    allow sql sink to fetch modules locally w/ fallback to hub download

--- rust-connectors/sinks/sql/src/transform.rs
+++ rust-connectors/sinks/sql/src/transform.rs
@@ -5,7 +5,9 @@ use fluvio_smartengine::{SmartEngine, SmartModuleChainInstance, SmartModuleConfi
 
 use fluvio_smartmodule::dataplane::smartmodule::SmartModuleInput;
 use std::collections::BTreeMap;
+use fluvio::Fluvio;
 use url::Url;
+use fluvio_connectors_common::opt::CommonConnectorOpt;
 
 const PARAM_WITH: &str = "with";
 
@@ -15,6 +17,7 @@ pub struct Transformations {
 }
 
 impl Transformations {
+    // TODO: move up to common crate and combine with from_fluvio method to allow mixed module sources
     pub async fn from_hub(
         hub_url: Url,
         value: Vec<TransformOpt>,
@@ -25,6 +28,7 @@ impl Transformations {
                 smart_module_chain: builder.initialize()?,
             });
         }
+
         let downloader = Downloader::from_url(hub_url)?;
         for step in value {
             let mut param: BTreeMap<String, String> = BTreeMap::new();
@@ -32,6 +36,37 @@ impl Transformations {
                 param.insert(PARAM_WITH.to_string(), with);
             }
             let raw = downloader.download_binary(step.uses.as_str()).await?;
+            let mut sm_builder = SmartModuleConfig::builder();
+            let sm_builder = sm_builder.params(param.into());
+
+            builder.add_smart_module(sm_builder.build()?, raw)?;
+        }
+
+        Ok(Self {
+            smart_module_chain: builder.initialize()?,
+        })
+    }
+
+    // TODO: move up to common crate
+    pub async fn from_fluvio(
+        value: Vec<TransformOpt>,
+    ) -> anyhow::Result<Transformations> {
+        let mut builder = SmartEngine::new().builder();
+        if value.is_empty() {
+            return Ok(Self {
+                smart_module_chain: builder.initialize()?,
+            });
+        }
+
+        let fluvio = Fluvio::connect().await?;
+
+        for step in value {
+            let mut param: BTreeMap<String, String> = BTreeMap::new();
+            if let Some(with) = step.with {
+                param.insert(PARAM_WITH.to_string(), with);
+            }
+            let raw = CommonConnectorOpt::default().get_smartmodule(&step.uses, &fluvio).await?;
+
             let mut sm_builder = SmartModuleConfig::builder();
             let sm_builder = sm_builder.params(param.into());
 
