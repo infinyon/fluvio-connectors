9245e92b22148a626b6cf06c4627f5d581fc0266
Nick Cardin <nick@cardin.email>
2022-10-19 22:38:30 +0000

    only init if OTEL_EXPORTER is set

--- rust-connectors/common/src/lib.rs
+++ rust-connectors/common/src/lib.rs
@@ -1,3 +1,5 @@
+use fluvio_future::tracing::{error, info};
+
 pub mod fluvio {
     pub use fluvio::{
         consumer::Record, metadata::topic::TopicSpec, Fluvio, FluvioError, Offset,
@@ -15,36 +17,43 @@ pub fn git_hash_version() -> &'static str {
 }
 
 pub fn init_open_telemetry(pkg_name: &str, pkg_version: &str) {
-    use opentelemetry::global;
     use opentelemetry::sdk::Resource;
     use opentelemetry::KeyValue;
     use std::time::Duration;
 
-    let resource_attributes = [
-        KeyValue::new("service.name", pkg_name.to_owned()),
-        KeyValue::new("service.version", pkg_version.to_owned()),
-    ];
-    let resource = Resource::new(resource_attributes);
+    match std::env::var("OTEL_EXPORTER") {
+        Ok(exporter) if (exporter == "otlp") || (exporter == "otlp_metric") => {
+            let resource_attributes = [
+                KeyValue::new("service.name", pkg_name.to_owned()),
+                KeyValue::new("service.version", pkg_version.to_owned()),
+            ];
+            let resource = Resource::new(resource_attributes);
 
-    let pusher = opentelemetry_otlp::new_pipeline()
-        .metrics(
-            opentelemetry::sdk::metrics::selectors::simple::inexpensive(),
-            opentelemetry::sdk::export::metrics::aggregation::cumulative_temporality_selector(),
-            opentelemetry::runtime::AsyncStd,
-        )
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
-        .with_period(Duration::from_secs(1))
-        .with_timeout(Duration::from_secs(10))
-        .with_resource(resource)
-        .build()
-        .unwrap();
-
-    global::set_meter_provider(pusher);
+            match opentelemetry_otlp::new_pipeline()
+                .metrics(
+                    opentelemetry::sdk::metrics::selectors::simple::inexpensive(),
+                    opentelemetry::sdk::export::metrics::aggregation::cumulative_temporality_selector(),
+                    opentelemetry::runtime::AsyncStd,
+                )
+                .with_exporter(opentelemetry_otlp::new_exporter().tonic())
+                .with_period(Duration::from_secs(1))
+                .with_timeout(Duration::from_secs(10))
+                .with_resource(resource)
+                .build() {
+                    Ok(_) => info!("initialized otlp metrics exporter"),
+                    Err(_) => error!("unable to initialize otlp metrics exporter")
+                }
+        }
+        _ => (),
+    }
 }
 
 #[macro_export]
 macro_rules! common_initialize {
     () => {
-        fluvio_connectors_common::init_open_telemetry(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        fluvio_connectors_common::init_open_telemetry(
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+        );
     };
 }
