4666fea6b60aa8be61f556ba521e33fe9f2562f9
Alexander Galibey <alexander.galibey@infinyon.com>
2022-10-31 17:19:33 +0000

    feat(smartmodule): added smartmodule chain for sink connectors (#397)
    Sink connectors switched to use Fluvio built-in SmartModue chain support.

--- rust-connectors/sinks/sql/src/main.rs
+++ rust-connectors/sinks/sql/src/main.rs
@@ -1,6 +1,5 @@
 use std::sync::Arc;
 
-use fluvio::dataplane::record::Record;
 use fluvio_connectors_common::metrics::ConnectorMetrics;
 use fluvio_connectors_common::monitoring::init_monitoring;
 use fluvio_future::tracing::{debug, info};
@@ -13,7 +12,6 @@ use fluvio_connectors_common::git_hash_version;
 use schemars::schema_for;
 use sql_sink::db::Db;
 use sql_sink::opt::SqlConnectorOpt;
-use sql_sink::transform::Transformations;
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
@@ -43,29 +41,14 @@ async fn main() -> anyhow::Result<()> {
     let mut stream = raw_opts.common.create_consumer_stream("sql").await?;
     info!("connected to fluvio stream");
 
-    let mut transformations = Transformations::from_chain(
-        raw_opts
-            .common
-            .transform_common
-            .create_smart_module_chain()
-            .await?
-            .unwrap_or_default(),
-    )
-    .await?;
-    debug!("{:?} transformations loaded", transformations);
-
     info!(
         "starting stream processing from {}",
         raw_opts.common.fluvio_topic
     );
     while let Some(Ok(consumer_record)) = stream.next().await {
-        let record: Record = consumer_record.into_inner();
-        let output = transformations.transform(record)?;
-        for output_record in output {
-            let operation: Operation = serde_json::from_slice(output_record.value.as_ref())?;
-            debug!("{:?}", operation);
-            db.execute(operation).await?;
-        }
+        let operation: Operation = serde_json::from_slice(consumer_record.as_ref())?;
+        debug!("{:?}", operation);
+        db.execute(operation).await?;
     }
 
     Ok(())
